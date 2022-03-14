37512dd2f6a88dbd1e4dedca36e2a0e319c3d0ea
David Ulloa <dulloa6310@gmail.com>
2022-03-14 12:27:26 -0400

    Added Art Contest 2022 Winner

--- pages/art-contest.js
+++ pages/art-contest.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Head from "next/head";
+import Image from "next/image";
 
 import PhotoGallery from "../components/art-contest/PhotoGallery";
 
@@ -15,17 +16,35 @@ function ArtContest(props) {
       <Head>
         <title>MYCS Website | Art Contest</title>
       </Head>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1 style={{ textAlign: "center" }}>
+          Congratulations to Nicole Ruiz for winning the MYCS 2022 Art Contest!
+        </h1>
+        <div style={{ maxWidth: "800px" }}>
+          <Image
+            alt="Nicole Ruiz Fish"
+            src={require("../images/art-contest-winner/nicole_ruiz_fish.PNG")}
+          ></Image>
+        </div>
+      </div>
       <h1 className={styles["title"]}>
         Take A Look At These Amazing Submissions from the 2021 Competition!
       </h1>
-      <h2 style={{ textAlign: "center" }}>Click for full image</h2>
-      <h2 style={{ textAlign: "center" }}>
-        2022 Art Contest Up Now! <br />
+      <h2 style={{ textAlign: "center", fontWeight: "400" }}>
+        Click for full image
+      </h2>
+      <h2 style={{ textAlign: "center", fontWeight: "400" }}>
         <a
           style={{ color: "#96dee1", textDecoration: "underline" }}
-          href="https://docs.google.com/forms/d/e/1FAIpQLSdlZrbunENEpJ3nN9wU4EoiMsXUQyFvuWfCID1SID0JakrS5A/viewform"
+          href="https://www.youtube.com/watch?v=jddi2ZVNGvo"
         >
-          Click Here To Submit Your Art
+          Click Here To See 2022 Submissions
         </a>
       </h2>
       <PhotoGallery></PhotoGallery>
--- pages/index.js
+++ pages/index.js
@@ -16,37 +16,11 @@ export default function Home() {
     document.getElementById("body").style.backgroundColor = "#96DEE1";
   }, []);
 
-  function toggleModal() {
-    setShowModal((prevState) => !prevState);
-  }
-
   return (
     <main>
       <Head>
         <title>MYCS Website | Index</title>
       </Head>
-      {showModal && (
-        <Modal onClick={toggleModal}>
-          <div className={styles["alert-modal"]}>
-            <h1 className={styles["close-modal"]}>X</h1>
-            <h1>Registration is now up!</h1>
-            <p>
-              To register, click the button below or go to this link:{" "}
-              <a href="https://whova.com/portal/registration/mycs_202203/">
-                https://whova.com/portal/registration/mycs_202203/
-              </a>
-            </p>
-            <Link
-              passHref
-              href="https://whova.com/portal/registration/mycs_202203/"
-            >
-              <Button style={{ width: "200px", fontSize: "1.5em" }}>
-                Register
-              </Button>
-            </Link>
-          </div>
-        </Modal>
-      )}
       <SkySection></SkySection>
       <GrassSection></GrassSection>
     </main>
